da0860523bbece95bb42e92da5c70ec55843b16b
thallesnct <thallescarvalho12@gmail.com>
2019-10-30 22:06:49 -0300

    [ch52] Adicionado tamanho de fontes dinamico à página

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import code from '../../assets/svg/code.svg';
 import {
   Container,
-  Top,
   Svg,
   Title,
   TextContainer,
@@ -13,22 +12,20 @@ import {
 function Home() {
   return (
     <Container>
-      <Top>
-        <TextContainer>
-          <Title>
-            Atinja o próximo nível de suas habilidades de criação de código
-          </Title>
-          <Paragraph>
-            Com a ajuda dos desafios propostos por nós da codr, exercite os seus
-            conhecimento em algoritmos e estruturas de dados, e receba feedback
-            sobre a sua performance. Além disso, batalhe contra outros
-            desenvolvedores para descobrir quem consegue chegar na solução de um
-            problema mais rapidamente!
-          </Paragraph>
-          <Button>ver desafios</Button>
-        </TextContainer>
-        <Svg src={code} alt="codes" />
-      </Top>
+      <TextContainer>
+        <Title>
+          Atinja o próximo nível de suas <br /> habilidades de criação de código
+        </Title>
+        <Paragraph>
+          Com a ajuda dos desafios propostos por nós da codr, exercite os seus
+          conhecimentos em algoritmos e estruturas de dados, e receba feedback
+          sobre a sua performance. Além disso, batalhe contra outros
+          desenvolvedores para descobrir quem consegue chegar na solução de um
+          problema mais rapidamente!
+        </Paragraph>
+        <Button to="/questions">ver desafios</Button>
+      </TextContainer>
+      <Svg src={code} alt="codes" />
     </Container>
   );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,21 +1,26 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
-  display: flex;
-  flex: 1;
+  display: grid;
+  grid-template-columns: 3fr 2fr;
+  grid-column-gap: 32px;
   height: 80vh;
-  padding: 0 5vw 48px;
+  padding: 48px 5vw;
   background-color: ${props =>
     props.theme === 'clear' ? '#f8f8f2' : '#44475a'};
-`;
 
-export const Top = styled.div`
-  padding-top: 5vh;
-  display: flex;
-  flex: 1;
-  align-items: flex-start;
-  flex-direction: row;
-  justify-content: space-between;
+  @media only screen and (max-width: 1024px) {
+    display: flex;
+    flex-direction: column-reverse;
+    justify-content: center;
+    align-items: center;
+    height: 82.5vh;
+  }
+
+  @media only screen and (max-width: 768px) {
+    height: unset;
+  }
 `;
 
 export const TextContainer = styled.div`
@@ -24,38 +29,65 @@ export const TextContainer = styled.div`
   flex: 1;
   flex-direction: column;
   align-items: flex-start;
-  justify-content: center;
+  margin-top: 40px;
+
+  @media only screen and (max-width: 1024px) {
+    margin-top: 20px;
+    align-items: center;
+  }
 `;
 
 export const Title = styled.h1`
   font-family: 'Comfortaa', sans-serif;
-  max-width: 45vw;
-  font-size: 36px;
+  font-size: calc(20px + 1vw);
   font-weight: 100;
   color: #bd93f9;
+  width: 80%;
+
+  @media only screen and (max-width: 1024px) {
+    text-align: center;
+    font-size: calc(14px + 1vw);
+    width: 100%;
+  }
 `;
 
-export const Button = styled.button`
+export const Button = styled(Link)`
+  text-decoration: none;
   font-family: 'Comfortaa', sans-serif;
-  font-size: 14px;
+  font-size: calc(10px + 0.295vw);
   background-color: #bd93f9;
   font-weight: bold;
   color: #44475a;
   border: none;
   border-radius: 1000vw;
-  padding: 0.4rem 2.4rem;
+  padding: 8px 48px;
   height: min-content;
-  margin-top: 2.4rem;
+
+  @media only screen and (max-width: 1024px) {
+    font-size: calc(10px + 1vw);
+  }
 `;
 
 export const Paragraph = styled.p`
-  max-width: 45vw;
-  line-height: 25px;
-  margin-top: 5vh;
+  line-height: 1.6;
+  margin: 40px 0 36px;
+  font-size: calc(10px + 0.295vw);
   color: #bd93f9;
+  width: 80%;
+
+  @media only screen and (max-width: 1024px) {
+    width: 100%;
+    font-size: calc(10px + 1vw);
+  }
 `;
 
 export const Svg = styled.img`
-  width: 500px;
+  max-width: 80%;
+  width: 80%;
   height: auto;
+  margin: 0 auto;
+
+  @media only screen and (max-width: 1024px) {
+    width: calc(100px + 30vw);
+  }
 `;
