baa4f17668a012ac7443578adf3ab95739dd56f9
roger067 <rosestolato.rogerio@gmail.com>
2019-12-11 22:25:47 -0300

    adding modal login

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
 import { Header } from './components/Header';
 import Footer from './components/Footer';
+import Modal from './components/Modal';
 
 function App() {
+  const [modalOpen, setModalOpen] = useState(false);
   return (
     <BrowserRouter>
-      <Header />
+      <Header setModalOpen={setModalOpen} />
       <GlobalStyle />
       <Routes />
       <Footer />
+      {modalOpen && <Modal setModalOpen={setModalOpen} />}
     </BrowserRouter>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Container, Logo, Nav, NavLink } from './styles';
 
-export function Header() {
+export function Header({ setModalOpen }) {
   return (
     <Container>
       <Logo to="/">codr</Logo>
       <Nav>
         <NavLink to="/questions">problemas</NavLink>
         <NavLink to="/questions">code battle</NavLink>
-        <NavLink to="/questions">entrar</NavLink>
+        <NavLink onClick={() => setModalOpen(true)}>entrar</NavLink>
       </Nav>
     </Container>
   );
